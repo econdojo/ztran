3695dd9241364b7aaa81542942cd1c454d680433
Fei Tan <tanf@slu.edu>
2022-09-10 10:16:48 -0500

    Update copyright

--- startup.m
+++ startup.m
@@ -2,7 +2,7 @@
 % |   _______ _____            _   _                                      |
 % |  |__   __|  __ \     /\   | \ | |   Welcome to z-Tran MATLAB Toolbox  |
 % |  ___| |  | |__) |   /  \  |  \| |                                     |
-% | |_  / |  |  _  /   / /\ \ | . ` |   (c) Zhao Han, Fei Tan & Jieran Wu |
+% | |_  / |  |  _  /   / /\ \ | . ` |   (c) Fei Tan & Jieran Wu           |
 % |  / /| |  | | \ \  / ____ \| |\  |   Last modified: 1-Oct-2021         |
 % | /___|_|  |_|  \_\/_/    \_\_| \_|   Contact: tanf@slu.edu             |
 % |                                                                       |
